230114381f09459dcdeaf42e39a97514672dbc1f
Demet Duyar <dduyar@k1m33s02.42istanbul.com.tr>
2025-04-13 15:49:02 +0300

    structs created and initialized

---
+++ src/utils.c
@@ -0,0 +1,36 @@
+#include "../inc/minishell.h"
+#include <stdlib.h>
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	if (!src)
+		return (NULL);
+	len = ft_strlen(src);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}

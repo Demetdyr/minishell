b424a448b72e94a06915269627d6dbeec122f0da
Efe-MEC <m.efeckr@gmail.com>
2025-06-25 20:00:53 +0300

    adding pwd cmd

--- src/exec_pwd.c
+++ src/exec_pwd.c
@@ -1,8 +1,13 @@
 #include "../inc/minishell.h"
+#include <unistd.h>
 
 int	ft_exec_pwd(t_shell *shell, t_cmd *cmd)
 {
-	(void)cmd;
-	(void)shell;
+	char	buffer[PATH_MAX];
+
+	if (getcwd(buffer, sizeof(buffer)) == 0 || ft_strlen(buffer) == 0)
+		return (FAILURE);
+	fdprintln(cmd->bout, buffer);
+	shell->status = 0;
 	return (SUCCESS);
 }

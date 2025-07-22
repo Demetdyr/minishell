2e3f0e0e99c59bea65d7d3e1c8ed15fb27ebf9f6
Efe-MEC <m.efeckr@gmail.com>
2025-07-22 19:15:49 +0300

    fd control  in print adding

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: mehcakir <mehcakir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 18:09:41 by dduyar            #+#    #+#             */
-/*   Updated: 2025/07/20 22:38:19 by mehcakir         ###   ########.fr       */
+/*   Updated: 2025/07/22 19:11:35 by mehcakir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 
 void	fdprint(int fd, const char *s)
 {
+	if (fd < 0)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
 
 void	fdprintln(int fd, const char *s)
 {
+	if (fd < 0)
+		return ;
 	fdprint(fd, s);
 	fdprint(fd, "\n");
 }
